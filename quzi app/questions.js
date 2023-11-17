0e70d46f269accd709f3ef6fc2009877299fd7d6
Dwarka prasad <69027937+Dpmahala@users.noreply.github.com>
2023-11-17 12:42:19 +0530

    Add files via upload

---
+++ quzi app/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question:"When Google was launched?",
+    answer:"1998",
+    options:[
+        "1998",
+        "2002",
+        "2000",
+        "2001"
+    ]
+},
+{
+    numb: 7,
+    question:"What is the Capital of Canada?",
+    answer:"Ottawa",
+    options:[
+        "Ottawa",
+        "Jordan",
+        "Jerusalem",
+        "New Jersey"
+    ]
+},
+{
+    numb: 8,
+    question:"What does Noida Stands for?",
+    answer:"New Okhla Industrial Development Authority",
+    options:[
+        "New Open Industrial Development Area",
+        "New Open Industry Development Authoriity",
+        "New Okhla Industrial Development Authority",
+        "New Okhla Industry Development Area"
+    ]
+},
+{
+    numb: 9,
+    question:"Where is the Eiffel Tower?",
+    answer:"Paris",
+    options:[
+        "Egypt",
+        "Paris",
+        "London",
+        "Canada"
+    ]
+},
+{
+    numb: 10,
+    question:"Safest place for Women in India?",
+    answer:"New Delhi",
+    options:[
+        "Haryana",
+        "New Delhi",
+        "Hyderbad",
+        "Rajasthan"
+    ]
+},
+{
+  numb: 11,
+  question:"What does HTML stand for?",
+  answer:"Hyper Text Markup Language",
+  options:[
+      "Hyper Text Processcor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multi Language",
+      "Hyper Text Multiple Language"
+  ]
+},
+{
+  numb: 12,
+  question:"What is the Capital of SriLanka?",
+  answer:"Colombo",
+  options:[
+      "Canberra",
+      "Colombo",
+      "Jerusalem",
+      "Jakarta"
+  ]
+},
+{
+  numb: 13,
+  question:"Which is the most hilly state in India?",
+  answer:"Himachal Pradesh",
+  options:[
+      "Jharkhand",
+      "Kerala",
+      "Maharashtra",
+      "Himachal Pradesh"
+  ]
+},
+{
+  numb: 14,
+  question:"Who directed Interstaller?",
+  answer:"Nolan",
+  options:[
+      "Nolan",
+      "Mackey",
+      "Kate Hudson",
+      "Jake jillenhol"
+  ]
+},
+{
+  numb: 15,
+  question:"Who discovered Aeroplane?",
+  answer:"W Brothers",
+  options:[
+      "W Brothers",
+      "G Marcony",
+      "J Mahajan",
+      "Thomson"
+  ]
+},
+{
+  numb: 16,
+  question:"What does www stands for?",
+  answer:"World Wide Web",
+  options:[
+      "World Web wide",
+      "World Wide Web",
+      "World Well World",
+      "Wrong World War"
+  ]
+},
+{
+  numb: 17,
+  question:"What is the Capital of Australia?",
+  answer:"Canberra",
+  options:[
+      "Congo",
+      "Canberra",
+      "Jerusalem",
+      "New Jersey"
+  ]
+},
+{
+  numb: 18,
+  question:"What does Noida https for?",
+  answer:"Hyper Text Transfer Protocol and secure",
+  options:[
+      "Hyper Text Transfer Protocol and secure",
+      "Hyper Text Travel Protocol and secure",
+      "Hyper Text Transfer Program and secure",
+      "Hyper Text Transfer Program and Services"
+  ]
+},
+{
+  numb: 19,
+  question:"Where is the Gateway of India?",
+  answer:"Paris",
+  options:[
+      "New Delhi",
+      "Maharashtra",
+      "Uttarakhand",
+      "Hyderabad"
+  ]
+},
+{
+  numb: 20,
+  question:"Cleanest city in India?",
+  answer:"Indore",
+  options:[
+      "Haryana",
+      "New Delhi",
+      "Indore",
+      "Rajasthan"
+  ]
+}
+];
